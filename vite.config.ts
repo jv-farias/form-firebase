c6916c7562ec4137710b64d1a874e8661f3641af
João Vitor Rodrigues Farias <84153149+jv-farias@users.noreply.github.com>
2023-11-15 17:06:27 +0000

    ajuste nos links

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,17 @@
-// vite.config.ts
+/// <reference types="vite/client" />
+/// <reference types="node" />
+
+import { resolve } from 'path';
 import { defineConfig } from 'vite'
 
-export default defineConfig({})
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/form-firebase/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+})
