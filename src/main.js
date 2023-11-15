debc0e004c4b471e7c83bf90cfb44c13c9e3de24
João Vitor Rodrigues Farias <84153149+jv-farias@users.noreply.github.com>
2023-11-15 15:43:54 +0000

    form concluido

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import firebase from "firebase/compat/app";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAM1mn3jJY86f0eTYixRDQU5sN3-SMYQE4",
@@ -9,27 +10,49 @@ const firebaseConfig = {
   appId: "1:183493609614:web:51fe50a7c9857380eda1df"
 };
 
-// Initialize Firebase
+// Inicializa o Firebase
 firebase.initializeApp(firebaseConfig);
 
 // Reference your database
 const facetofaceDB = firebase.database().ref('facetoface')
 
-document.getElementById('dadosForm').addEventListener('submit', submitForm);
+/**
+ * @type {HTMLElement}
+ */
+
+document.querySelector('#dadosForm').addEventListener('submit', submitForm);
+
+/**
+ * 
+ * @param {Event} e 
+ */
 
 function submitForm(e) {
   e.preventDefault();
 
-  var name = getElementValue('name');
-  var emailid = getElementValue('emailid');
-  var msgContent = getElementValue('msgContent');
+  let name = getElementValue('name');
+  let age = getElementValue('age');
+  let role = getElementValue('role');
+  let social_link = getElementValue('social_link');
+  let hobby = getElementValue('hobby');
+  let avatar = getElementValue('avatar');
 
-  saveMessage(name, emailid, msgContent);
+  saveMessage(name, age, role, social_link, hobby, avatar);
+
+  /**
+   * @type {HTMLElement} Elemento DOM com a classe 'alert'.
+   * 
+   */
 
   document.querySelector('.alert').style.display = 'block';
 
   //remove the alert 
   setTimeout(() => {
+
+    /**
+  * @type {HTMLElement} Elemento DOM com a classe 'alert'.
+  */
+
     document.querySelector('.alert').style.display = 'none';
   }, 3000);
 
@@ -37,15 +60,38 @@ function submitForm(e) {
 }
 
 
-const saveMessage = (name, email, msgContent) => {
+/**
+ * 
+ * @param {string} name 
+ * @param {number} age 
+ * @param {string} role 
+ * @param {string} social_link 
+ * @param {string} hobby 
+ * @param {string} avatar 
+ */
+
+const saveMessage = (name, age, role, social_link, hobby, avatar) => {
   const newUserForm = facetofaceDB.push();
   newUserForm.set({
     name: name,
-    emailid: email,
-    msgContent: msgContent
+    age: age,
+    role: role,
+    social_link: social_link,
+    hobby: hobby,
+    avatar: avatar
   })
 }
 
+/**
+ * 
+ * @param {*} id 
+ * @returns 
+ */
+
 const getElementValue = (id) => {
+
+  /**
+   * @type {HTMLInputElement}
+   */
   return document.getElementById(id).value
 }
